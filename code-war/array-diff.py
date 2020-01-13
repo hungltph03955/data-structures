cfc08ba05508730c686d2112b41a121a296909d0
tuanhungluong <tuanhungluong1602@gmail.com>
2020-01-14 02:08:29 +0700

    fix

--- code-war/array-diff.py
+++ code-war/array-diff.py
@@ -4,34 +4,45 @@ It should remove all values from list a, which are present in list b.
 array_diff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 array_diff([1,2,2,2,3],[2]) == [1,3]
+
+array_diff([1,2,2,2,3,5,5,6,7],[2,6]) == [1,3,5,5,7]
+"""
+"""
+Besst solution :
+    def array_diff(a, b):
+        return [x for x in a if x not in set(b)]
+
 """
 
 
-def array_diff(array_raw, array_to_cut):
-    array_unique = []
-    if len(array_to_cut) > 0:
+def array_to_duplicate_diff(array_raw, array_to_cut):
+    if len(array_to_cut) == 1:
+        array_beauty = []
+        i = 0
+        while i < len(array_raw):
+            if array_raw[i] != array_to_cut[0]:
+                array_beauty.append(array_raw[i])
+            i += 1
+        return array_beauty
+    elif len(array_to_cut) > 1:
         i = 0
         while i < len(array_raw):
             k = 0
             while k < len(array_to_cut):
-                if array_raw[i] != array_to_cut[k]:
-                    if len(array_unique) > 0:
-                        l = 0
-                        while l < len(array_unique):
-                            if array_raw[i] != array_unique[l]:
-                                array_unique.append(array_raw[i])
-                            l += 1
-                    else:
-                        array_unique.append(array_raw[i])
+                if array_raw[i] == array_to_cut[k]:
+                    array_raw[i] = "unique"
                 k += 1
             i += 1
+        return array_raw
     else:
-        array_unique = array_raw
-
-    return array_unique
+        return array_raw
 
 
-array_raw = [1, 1, 2, 3, 4, 5, 2, 2, 5, 5]
-array_to_cut = [2, 5]
-result = array_diff(array_raw, array_to_cut)
+def array_diff(array_raw, array_to_cut):
+    array_to_clean = array_to_duplicate_diff(array_raw, array_to_cut)
+    return array_to_clean
+
+
+result = array_diff([-12, -1, 15, 0, -16, 0, -6,
+                     11, -7, -7, 15, 20], [-7, -5, 17])
 print(result)
