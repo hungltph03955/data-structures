f15f2c9c146fb7645a6aa32e8fd94905e8c8348e
tuanhung <mac@MacOnes-MacBook-Pro.local>
2020-01-25 22:23:07 +0700

    code

--- code-war/filter_list.py
+++ code-war/filter_list.py
@@ -11,8 +11,20 @@ test.assert_equals(filter_list([1,2,'a','b']),[1,2])
 test.assert_equals(filter_list([1,'a','b',0,15]),[1,0,15])
 test.assert_equals(filter_list([1,2,'aasf','1','123',123]),[1,2,123])
 
+best solution:
+
+def filter_list(l):
+  'return a new list with the strings filtered out'
+  return [i for i in l if not isinstance(i, str)]
+
 """
 
 
-def filter_list(l):
-    'return a new list with the strings filtered out'
+def filter_list(test):
+    return [x for x in test if type(x) != str]
+
+
+array_test = [1, 2, 'aasf', '1', '123', 123]
+
+result = filter_list(array_test)
+print(result)
